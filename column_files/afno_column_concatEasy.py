e77231e35939ea7119cf386083968039a289f0c5
Yves <yves_bicker@icloud.com>
2024-09-19 15:17:12 +0000

    update

--- column_files/afno_column_concatEasy.py
+++ column_files/afno_column_concatEasy.py
@@ -112,7 +112,6 @@ class Block(nn.Module):
     
         mlp_hidden_dim = int(dim * mlp_ratio)
         self.mlp = Mlp(in_features=dim, hidden_features=mlp_hidden_dim, act_layer=act_layer, drop=drop)
-
         self.double_skip = double_skip
 
     def forward(self, x):
